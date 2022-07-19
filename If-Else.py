92202888dd92a205cdf31530e080e20e0e119614
m0rbot <98833762+m0rbot@users.noreply.github.com>
2022-07-19 09:45:23 -0700

    Update If-Else.py
    add some explanation about inclusive and exclusive range

--- If-Else.py
+++ If-Else.py
@@ -1,10 +1,10 @@
 n = int(input())
-#1 to 10 inclusive: 1, 2, 3, 4, 5, 6, 7, 8, 9, 10
-#1 to 10 exclusive: 1, 2, 3, 4, 5, 6, 7, 8, 9
+#1 to 10 inclusive: 1, 2, 3, 4, 5, 6, 7, 8, 9, 10.
+#1 to 10 exclusive: 1, 2, 3, 4, 5, 6, 7, 8, 9.
 if n%2!=0:
     print("Weird")
 else:
-    if n in range(2, 6): #range is an exclusive func, rang(2, 5) -> 2,3,4, we should to type range(2, 6) to make an inclusive range of 2 to 5
+    if n in range(2, 6): #range is an exclusive func, rang(2, 5) -> 2,3,4, we should to type range(2, 6) to make an inclusive range of 2 to 5.
         print("Not Weird")
     elif n in range(6, 21):
         print("Weird")
